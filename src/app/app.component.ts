62099b3fa1b858804c93186582374e9e3f9d8c4e
Hari Mahadevan <hari@smallpearl.com>
2022-02-24 11:41:34 +0800

    Install & initialize @capacitor-community/sqlite & jeep-sqlite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { SQLiteService } from './services/sqlite.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  private initPlugin = false;
+  constructor(private platform: Platform, private sqlite: SQLiteService) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(async () => {
+      await customElements.whenDefined('jeep-sqlite');
+      this.sqlite.initializePlugin().then(async (ret) => {
+        this.initPlugin = ret;
+        console.log('>>>> in App  this.initPlugin ' + this.initPlugin);
+      });
+    });
+  }
 }
